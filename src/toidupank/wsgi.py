8f1347442ec7e6bcfb23c889c720dd7dc7d8896e
Mart Somermaa <mart.somermaa@cgi.com>
2016-06-24 21:57:59 +0300

    Fix wsgi.py

--- src/toidupank/wsgi.py
+++ src/toidupank/wsgi.py
@@ -7,9 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(BASE_DIR)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "toidupank.settings")
 
