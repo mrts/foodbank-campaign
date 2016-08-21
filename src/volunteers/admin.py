cee29d28dc75e5acb3d8a2f572dc822453e42e3c
Mart Sõmermaa <mart.somermaa@gmail.com>
2016-08-21 12:45:54 +0300

    Make volunteers admin more usable

--- src/volunteers/admin.py
+++ src/volunteers/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from volunteers.models import Volunteer
 
 class VolunteerAdmin(admin.ModelAdmin):
-    search_fields = ['name']
+    search_fields = ['first_name', 'last_name', 'email', 'phone']
+    list_display = ['first_name', 'last_name', 'email', 'phone', 'age']
 
 admin.site.register(Volunteer, VolunteerAdmin)
