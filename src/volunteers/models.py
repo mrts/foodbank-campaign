a535f7b09dccbe2c976b6c3294d69a6c2870f6d0
Mart Sõmermaa <mart.somermaa@gmail.com>
2016-10-28 20:46:16 +0300

    s/Is group/Is group representative

--- src/volunteers/models.py
+++ src/volunteers/models.py
@@ -9,7 +9,7 @@ class Volunteer(models.Model):
     age = models.PositiveIntegerField(_('Age'))
     phone = models.CharField(_('Phone'), max_length=100)
     email = models.EmailField(_('E-mail'), unique=True)
-    is_group = models.BooleanField(_('Is group'), default=False)
+    is_group = models.BooleanField(_('Is group representative'), default=False)
     group_name = models.CharField(_('Group/organization name'), max_length=100,
             blank=True)
     participant_count = models.PositiveIntegerField(_('Participant count'),
