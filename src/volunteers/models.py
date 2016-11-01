5527cbaca413f24e7b088bbbba023a769a30b2ff
Mart Sõmermaa <mart.somermaa@gmail.com>
2016-11-02 00:26:30 +0200

    Reorder group registration fields, add JS validation, allow registration of multiple groups under same email

--- src/volunteers/models.py
+++ src/volunteers/models.py
@@ -4,20 +4,21 @@ from django.core.validators import MinValueValidator
 
 
 class Volunteer(models.Model):
-    first_name = models.CharField(_('First name'), max_length=100)
-    last_name = models.CharField(_('Last name'), max_length=100)
-    age = models.PositiveIntegerField(_('Age'))
-    phone = models.CharField(_('Phone'), max_length=100)
-    email = models.EmailField(_('E-mail'), unique=True)
-    is_group = models.BooleanField(_('Is group representative'), default=False)
     group_name = models.CharField(_('Group/organization name'), max_length=100,
             blank=True)
     participant_count = models.PositiveIntegerField(_('Participant count'),
             default=1, validators=[MinValueValidator(1)])
+    first_name = models.CharField(_('First name'), max_length=100)
+    last_name = models.CharField(_('Last name'), max_length=100)
+    age = models.PositiveIntegerField(_('Age'), blank=True, null=True)
+    phone = models.CharField(_('Phone'), max_length=100)
+    email = models.EmailField(_('E-mail'))
+    is_group = models.BooleanField(_('Is group representative'), default=False)
 
     class Meta:
         verbose_name = _('Volunteer')
         verbose_name_plural = _('Volunteers')
+        unique_together = ['email', 'group_name']
 
     @property
     def name(self):
