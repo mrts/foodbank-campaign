b071a3304f8ef3b676d0f6198efd30c97e018055
Mart Sõmermaa <mart.somermaa@gmail.com>
2016-11-19 02:23:59 +0200

    Add statistics to locations admin

--- src/locations/admin.py
+++ src/locations/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db.models import F, Sum, IntegerField
+from django.utils.translation import ugettext_lazy as _
 
 import nested_admin
 
@@ -15,9 +17,23 @@ class CampaignShiftInline(nested_admin.NestedTabularInline):
 
 
 class LocationAdmin(nested_admin.NestedModelAdmin):
-    search_fields = ['name', 'address']
+    search_fields = ['name', 'address',
+            'campaignlocationshift__volunteers__first_name',
+            'campaignlocationshift__volunteers__last_name']
     list_filter = ['district']
     inlines = [CampaignShiftInline]
+    list_display = ['name', 'volunteers_count', 'free_places']
+
+    def volunteers_count(self, obj):
+        return obj.campaignlocationshift_set.aggregate(
+                count=Sum('volunteers__participant_count'))['count']
+    volunteers_count.short_description = _('Volunteers count')
+
+    def free_places(self, obj):
+        return obj.campaignlocationshift_set.aggregate(
+                places=F('total_places') - Sum('volunteers__participant_count',
+                    output_field=IntegerField()))['places']
+    free_places.short_description = _('Free places')
 
 
 admin.site.register(District)
