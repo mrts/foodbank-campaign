7e0030eb22671897a80633d57056ba0f26f15a77
Mart Sõmermaa <mart.somermaa@gmail.com>
2016-11-22 15:55:30 +0200

    Make filter_by_district more strict - don't show anything to unconfigured users

--- src/coordinators/models.py
+++ src/coordinators/models.py
@@ -13,10 +13,13 @@ class Coordinator(models.Model):
             blank=True, null=True)
 
 def filter_by_district(qs, user, lookup):
+    # superusers and managers see everything
     if (user.is_superuser
-            or not hasattr(user, 'coordinator')
-            or user.coordinator.is_manager):
+            or hasattr(user, 'coordinator') and user.coordinator.is_manager):
         return qs
+    # don't show anything to unconfigured users
+    if not hasattr(user, 'coordinator') or not user.coordinator.district:
+        return qs.none()
     kwargs = {
         lookup: user.coordinator.district
     }
