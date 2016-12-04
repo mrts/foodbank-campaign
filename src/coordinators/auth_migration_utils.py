a18e49002f58d06c96cfcd7deb3507ee260f2e19
Mart Sõmermaa <mart.somermaa@gmail.com>
2016-12-05 01:03:55 +0200

    Add shift participation CRUD rights to coordinators group

---
+++ src/coordinators/auth_migration_utils.py
@@ -0,0 +1,18 @@
+def get_coordinators_group(apps):
+    Group = apps.get_model('auth', 'Group')
+    return Group.objects.get(name='Koordinaatorid')
+
+def get_permissions(apps, *args):
+    Permission = apps.get_model('auth', 'Permission')
+    permissions = []
+    for app_name, model_name, prefixes in args:
+        permissions.extend(
+            get_permission_objects(Permission, app_name, model_name, prefixes)
+        )
+    return permissions
+
+def get_permission_objects(Permission, app_name, model_name, prefixes):
+    permission_names = map(lambda prefix: prefix + '_' + model_name, prefixes)
+    return list(Permission.objects.filter(
+            codename__in=permission_names,
+            content_type__app_label=app_name))
--- src/coordinators/migrations/0002_auto_20161127_1403.py
+++ src/coordinators/migrations/0002_auto_20161127_1403.py
@@ -4,22 +4,16 @@ from __future__ import unicode_literals
 
 from django.db import migrations
 
+from coordinators.auth_migration_utils import get_coordinators_group, get_permissions
+
+
 # TODO: should add rights first with a migration :)
 def remove_location_and_shift_add_delete_rights(apps, schema_editor):
-    Group = apps.get_model('auth', 'Group')
-    Permission = apps.get_model('auth', 'Permission')
-    coordinators_group = Group.objects.get(name='Koordinaatorid')
-    permissions = (
-            get_permissions(Permission, 'location', 'locations') +
-            get_permissions(Permission, 'campaignlocationshift', 'campaigns')
-    )
+    coordinators_group = get_coordinators_group(apps)
+    permissions = get_permissions(apps,
+            ('locations', 'location', ('add', 'delete')),
+            ('campaigns', 'campaignlocationshift', ('add', 'delete')))
     coordinators_group.permissions.remove(*permissions)
-
-def get_permissions(Permission, model_name, app_name):
-    permission_names = ['add_' + model_name, 'delete_' + model_name]
-    return list(Permission.objects.filter(
-            codename__in=permission_names,
-            content_type__app_label=app_name))
 
 
 class Migration(migrations.Migration):
