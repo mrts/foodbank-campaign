7a080696c5fdb72c758fceb49d3fe46b705d397c
Mart Sõmermaa <mart.somermaa@gmail.com>
2016-12-04 14:25:41 +0200

    Update locations, shift and volunteers summary view and add support for printing by page

--- src/locations/admin_actions.py
+++ src/locations/admin_actions.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.utils.translation import ugettext_lazy as _
 
 def list_volunteers_by_shift_and_location(modeladmin, request, queryset):
     return render(request,
             'locations/list_volunteers_by_shift_and_location.html',
             context = {'locations': queryset})
-list_volunteers_by_shift_and_location.short_description = 'List of volunteers by location and shift'
+list_volunteers_by_shift_and_location.short_description = _('List of volunteers by location and shift')
