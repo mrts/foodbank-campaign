7ea3421b715bf78087ef357f90277308a82763c0
Ott Kärner <ottkarner@gmail.com>
2018-03-24 23:24:20 +0200

    Add possibility to use sensitive data and add test to login into admin main page which uses sensitive data

--- tests/cypress/README.md
+++ tests/cypress/README.md
@@ -9,3 +9,12 @@ Run Cypress:
     node_modules/.bin/cypress open
 
 Press *Run All Tests* or click the test you wish to run
+
+## Passing sensitive data to tests
+
+Pass sensitive data with environment variables with *cypress.env.json* file:
+    
+    {
+        "admin_username": "user",
+        "admin_password": "password"
+    }
--- tests/cypress/cypress/integration/test_environment_marker_spec.js
+++ tests/cypress/cypress/integration/test_environment_marker_spec.js
@@ -4,8 +4,16 @@ describe('Test environment should have descriptive "Test" mark', function () {
     cy.get('.page-header > h2:nth-child(2)').should('contain', 'TEST')
   })
 
-  it('Check the admin page "Test" mark', function () {
+  it('Check the admin login page "Test" mark', function () {
     cy.visit('https://test-osale.toidupank.ee/haldus/')
+    cy.get('#site-name > a:nth-child(1) > span:nth-child(1)').should('contain', 'TEST')
+  })
+
+  it('Check the admin main page "Test" mark', function () {
+    cy.visit('https://test-osale.toidupank.ee/haldus/')
+    cy.get('#id_username').type(Cypress.env('admin_username'))
+    cy.get('#id_password').type(Cypress.env('admin_password'))
+    cy.get('.submit-row > input[type="submit"]').click()
     cy.get('#site-name > a:nth-child(1) > span:nth-child(1)').should('contain', 'TEST')
   })
 })
