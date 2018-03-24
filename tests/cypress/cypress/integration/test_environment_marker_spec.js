bef85414a04b2d66e0f9bc7beea4ccaf14c952b0
Ott Kärner <ottkarner@gmail.com>
2018-03-24 22:40:20 +0200

    Check the admin login page "TEST" mark

--- tests/cypress/cypress/integration/test_environment_marker_spec.js
+++ tests/cypress/cypress/integration/test_environment_marker_spec.js
@@ -3,9 +3,9 @@ describe('Test environment should have descriptive "Test" mark', function () {
     cy.visit('https://test-osale.toidupank.ee/')
     cy.get('.page-header > h2:nth-child(2)').should('contain', 'TEST')
   })
-  
-  //it('Check the admin page "Test" mark', function () {
-  //  cy.visit('https://test-osale.toidupank.ee/')
-  //  cy.title().should('include', 'Kitchen Sink')
-  //})
+
+  it('Check the admin page "Test" mark', function () {
+    cy.visit('https://test-osale.toidupank.ee/haldus/')
+    cy.get('#site-name > a:nth-child(1) > span:nth-child(1)').should('contain', 'TEST')
+  })
 })
